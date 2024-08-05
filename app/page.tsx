340335b9e56074b1f387a425b41b6e7bfb327082
) <raducea.matei2005@gmail.com>
2024-08-05 13:47:49 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
-import Image, { StaticImageData } from 'next/image';
-const ContactItem = ({ icon, text, link }: { icon: string, text: string, link?: string }) => (
-  <div className="flex items-stretch mb-4">
-    <div 
-      className="flex-shrink-0 w-12 h-12 mr-4 relative" 
-      style={{ userSelect: 'none' }} 
-      draggable="false"
-    >
-      <Image src={icon} alt="" width={50} height={50} priority draggable="false" />
-    </div>
-    <div className="flex-grow flex items-center min-h-[3rem]">
-      {link ? (
-        <a href={link} className="text-lg text-yellow-400 hover:text-yellow-300 underline" target="_blank" rel="noopener noreferrer">
-          {text}
-        </a>
-      ) : (
-        <p className="text-lg my-auto">{text}</p>
-      )}
-    </div>
-  </div>
-);
-
-export default function Contact() {
+export default function Home() {
   return (
     <div className="flex-grow flex items-center justify-center">
       <main className="text-center p-4">
-        <h1 className="text-4xl font-bold mb-4">contacts</h1>
-        <ContactItem icon="./imgs/phone.jpg" text="my phonee number: 0765823655" />
-        <ContactItem icon="./imgs/mail.jpg" text="my email: raducea.matei2005@gmail.com" />
-        <ContactItem icon="./imgs/github.jpg" text="my github repositories" link="https://github.com/Jupyyter?tab=repositories" />
+        <h1 className="text-4xl font-bold mb-4 py-1">nepune place</h1>
+        <p className="text-lg py-1">
+          i use this site to represent myself on the internet and showcase my projects. nothing much
+        </p>
       </main>
     </div>
-  );
+  )
 }
